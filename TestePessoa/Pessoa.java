8edb50e865fab795f482a5daab78632ea99743cc
Silvia DS Neri <168588457+Silvia-N27@users.noreply.github.com>
2024-05-06 19:12:54 -0300

    First Commit

---
+++ TestePessoa/Pessoa.java
@@ -0,0 +1,7 @@
+public class Pessoa { //definir uma classe pra pessoa
+    String nome, telefone;
+    
+    public void ExibeDados(){
+        System.out.println("Nome:"+nome+"\nTelefone:"+telefone); //printar os dados das pessoas
+    }
+}
---
+++ TestePessoa/TestePessoa.java
@@ -0,0 +1,9 @@
+public class TestePessoa{
+    public static void main(String[] args) {
+        Pessoa pessoaUm=new Pessoa(); //criar uma variavel pessoa
+        pessoaUm.nome="Josildo";
+        pessoaUm.telefone="42 999998889";
+        pessoaUm.ExibeDados();  //puxa a função para exibir os dados
+
+    }
+}
